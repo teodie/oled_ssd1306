111efcad2c389bce657994fd76238eab0fa33a3c
teodi <teodulosoriano07@gmail.com>
2023-08-23 11:27:39 +0800

    Updated the command variables

--- ssd1306.py
+++ ssd1306.py
@@ -26,8 +26,16 @@ SET_PAGE_ADDRESS_VH_MODE = 0x22          # START ADDRESS  A(0x00 ~ 0x07) | END B
 """Hardware Configuratitons"""
 SET_DISPLAY_START_LINE = 0x40            # 0x40 ~ 0x7F | RAM DISPLAY LINE REGISTER FROM 0-63 
 SET_SEGMENT_RE-MAP = 0xA0                # 0xA0/0xA1 | COLUMN ADDRESS 0 = SEG 0 / COLUMN ADDRESS 127 = SEG 0
-SET_MULTIPLEX_RATIO = 0xA8               # 
+SET_MULTIPLEX_RATIO = 0xA8               # A(15d-63d) SET MUX RATIO N+1 MUX FROM 16 MUX TO 64 MUX
+SET_COM_OUTPUT_SCAN = 0x0C               # 0x0C/0x08 | NORMAL FROM COM0 TO COM[N-1]/ REMAP COM[N-1] TO COM0 | N is the Multiplex ratio
+SET_DISPLAY_OFF_SET = 0xD3               # A(0d ~ 63d) | VERTICAL SHIFT BY COM 0d-63d
+SET_COM_PINS = 0xDA                      # A(0x02 ~ 0x32) | REFER TO THE DATASHEET
 
+"""Timing and Driving Scheme Setting"""
+SET_DISPLAY_CLOCK = 0xD5                 # A(0x00 ~ 0xFF) REFER TO THE DATASHEET
+SET_PRE_CHARGE_PERIOD = 0xD9             # A(0x00 ~ 0xFF) REFER TO THE DATASHEET
+SET_VCOM_DESELECT_LEVEL = 0xDB           # A(0x00h ~ 0x30h)
+NOP = 0xE3                               # NO OPERATION
 
 def display_initialization():
     pass
